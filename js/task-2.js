811ffdb0c34db92223dca9fc9b476153240e7419
Olyamh <olyamh234@yahoo.com>
2024-11-02 08:24:28 +0200

    hw-6 refine

--- js/task-2.js
+++ js/task-2.js
@@ -1,40 +1,40 @@
-// class Storage {
-//     #item;
+class Storage {
+    #item;
 
-//     constructor(params){
-//         this.#item = params;
-//     }
+    constructor(params){
+        this.#item = params;
+    }
 
-//     getItems() {
-//         return this.#item;
-//     }
+    getItems() {
+        return this.#item;
+    }
 
-//     addItem(newItem){
-//         this.#item.push(newItem);
-//         return this.#item;
-//     }
+    addItem(newItem){
+        this.#item.push(newItem);
+        return this.#item;
+    }
 
-//     removeItem(itemToRemove){
-//          const indOfItemToRemove = this.#item.indexOf(itemToRemove);
-//          if(!this.#item.includes(itemToRemove)){
-//             return this.#item;
-//          }
-//          const newItems = this.#item.splice(indOfItemToRemove, 1);
-//          return newItems;
+    removeItem(itemToRemove){
+         const indOfItemToRemove = this.#item.indexOf(itemToRemove);
+         if(indOfItemToRemove === -1){
+            return this.#item;
+         }
+         this.#item.splice(indOfItemToRemove, 1);
+         return this.#item;
                 
-//     }
+    }
 
 
-// }
+}
 
-// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
-// // console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
 
-// storage.addItem("Droid");
-// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
 
-// storage.removeItem("Prolonger");
-// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
 
-// storage.removeItem("Scaner");
-// console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+storage.removeItem("Scaner");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
